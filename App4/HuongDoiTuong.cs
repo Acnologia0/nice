80da82f22ccd098d47c3a0c1a3c9ff996d2b9ac4
Acnologia0 <70457497+Acnologia0@users.noreply.github.com>
2023-05-09 10:14:58 +0700

    da

---
+++ App4/HuongDoiTuong.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    public class HuongDoiTuong
+    {
+        public class DoanhThu
+        {
+            public int TongThu { get; set; }
+            public int TongChi { get; set; }
+            public int TienLoi => TongThu - TongChi;
+        }
+        public class SanPham
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public float GiaBan { get; set; }
+            public float GiaNhap { get; set; }
+            public float TienLoi => GiaBan - GiaNhap;
+            public int[] loais { get; set; }
+            public int[] thanhphan { get; set; }
+            public int[] dinhduong { get; set; }
+            public SanPham() 
+            {
+
+            }
+        }
+
+        public class DonDatHang
+        {
+            public int ID { get; set; }
+            public int[] sanpham { get; set; }
+            public string note { get; set; }
+            public bool dahoanthanh { get; set; }
+            public DateTime ngaydathang { get; set; }
+            public DateTime ngayhoanthanh { get; set; }
+        }
+        public class KhachHang
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string SDT { get; set; }
+            public int DonDatHang { get; set; }
+        }
+        public class Loai
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class ThanhPhan
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+        public class DinhDuong
+        {
+            public int ID { get; set; }
+            public string Name
+            {
+                get; set;
+            } 
+        }    
+    }
+}
--- App4/MainWindow.xaml.cs
+++ App4/MainWindow.xaml.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -16,6 +17,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,5 +31,47 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         this.InitializeComponent();     
-    } 
+    }
+
+    public ObservableCollection<HuongDoiTuong.KhachHang> khachhangs { get; set; } = new();
+    public ObservableCollection<HuongDoiTuong.SanPham> sanphams { get; set; } = new();
+    public ObservableCollection<HuongDoiTuong.DinhDuong> dinhduongs { get; set; } = new();
+    public ObservableCollection<HuongDoiTuong.ThanhPhan> thanhphans { get; set; } = new();
+    public ObservableCollection<HuongDoiTuong.Loai> loais { get; set; } = new();
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        AddKhachHang();
+    }
+
+    public void LayDuLieu()
+    {
+        AddKhachHang();
+        AddSanPham();
+        AddDinhDuong();
+        AddThanhPhan();
+        AddLoai();
+
+    }
+    public void AddKhachHang()
+    {
+        var khachhang = new HuongDoiTuong.KhachHang();
+        khachhang.ID = khachhangs.Count;
+        khachhangs.Add(khachhang);
+    }
+    public void AddSanPham()
+    {
+
+    }
+    public void AddDinhDuong()
+    {
+
+    }
+    public void AddThanhPhan()
+    {
+
+    }
+    public void AddLoai()
+    {
+
+    }
 }
